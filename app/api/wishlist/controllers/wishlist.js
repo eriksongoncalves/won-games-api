a25cecc7821b48e9dbef7d8775c2d7d3227d069d
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-06-23 16:37:14 -0300

    fix: sanitizeEntity

--- app/api/wishlist/controllers/wishlist.js
+++ app/api/wishlist/controllers/wishlist.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { sanitizeEntity } = require("strapi-utils");
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
